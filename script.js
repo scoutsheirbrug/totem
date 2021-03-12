7467ccb2b81d7abd9cd6659737b2f9b0c2704287
mstuyven <mirostuyven@gmail.com>
2021-03-12 18:56:12 +0100

    Move selected list

--- script.js
+++ script.js
@@ -17,15 +17,21 @@ let selectedChars = new Set()
 		document.querySelector('.chars-list').childNodes.forEach(e => {
 			e.classList.toggle('selected', selectedChars.has(e.textContent))
 		})
-		document.querySelector('.selected-chars-list').innerHTML = ''
+		document.querySelector('.selected-list').innerHTML = ''
 		;[...selectedChars].sort().forEach(c => {
 			const div = document.createElement('div')
-			div.addEventListener('click', () => {
+			const text = document.createElement('span')
+			text.textContent = c
+			const remove = document.createElement('div')
+			remove.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16"><path fill-rule="evenodd" d="M3.72 3.72a.75.75 0 011.06 0L8 6.94l3.22-3.22a.75.75 0 111.06 1.06L9.06 8l3.22 3.22a.75.75 0 11-1.06 1.06L8 9.06l-3.22 3.22a.75.75 0 01-1.06-1.06L6.94 8 3.72 4.78a.75.75 0 010-1.06z"></path></svg>'
+			remove.classList.add('remove')
+			remove.addEventListener('click', () => {
 				selectedChars.delete(c)
 				updateAnimals()
 			})
-			div.textContent = c
-			document.querySelector('.selected-chars-list').append(div)
+			div.append(remove)
+			div.append(text)
+			document.querySelector('.selected-list').append(div)
 		})
 
 		document.querySelector('.animals-list').scrollTo({ top: 0, behavior: 'smooth' })
@@ -46,7 +52,7 @@ let selectedChars = new Set()
 						${maxScore > 0 ? `<div class="animal-score" style="color: ${color}">${(a.score * 100).toFixed()}</div>` : ''}
 						<h2>${a.name}</h2>
 						${a.synonyms ? `<h3>${a.synonyms.join(', ')}</h3>` : ''}
-						<h4>${a.characteristics.join(' · ')}</h4>
+						<h4>${a.characteristics.map(c => selectedChars.has(c) ? `<span class="selected">${c}</span>` : c).join(' · ')}</h4>
 						<p>${a.description}</p>
 					</div>
 				`)
